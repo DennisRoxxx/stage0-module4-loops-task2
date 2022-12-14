3f9979a850bb0ad2842b44bfbb1e9beec8cd87dd
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-14 15:37:06 +0300

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -3,5 +3,7 @@ package school.mjc.stage0.loops.task2;
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
 
+        chars.toString();
+        System.out.println(chars);
     }
 }
